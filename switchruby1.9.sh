3aa44ae4feea868084c12cce2b62c811347e1323
gaoming <gao_ming_1990@163.com>
2017-03-01 16:04:22 +0800

    first jeklly server

---
+++ switchruby1.9.sh
@@ -0,0 +1,8 @@
+cd /usr/bin/
+ln -sf ruby1.9.1 ruby
+ln -sf gem1.9.1 gem
+ln -sf erb1.9.1 erb
+ln -sf irb1.9.1 irb
+ln -sf rake1.9.1 rake
+ln -sf rdoc1.9.1 rdoc
+ln -sf testrb1.9.1 testrb
---
+++ switchruby2.0.sh
@@ -0,0 +1,8 @@
+cd /usr/bin 
+ln -sf ruby2.0 ruby 
+ln -sf gem2.0 gem 
+ln -sf erb2.0 erb 
+ln -sf irb2.0 irb 
+ln -sf rake2.0 rake 
+ln -sf rdoc2.0 rdoc 
+ln -sf testrb2.0 testr 
